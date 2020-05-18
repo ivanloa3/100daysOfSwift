f038769941c6e3449e3d8cba47e5846394a11c54
Ivan Lopez Ansaldo <ivan.lopez@globant.com>
2020-05-18 12:10:37 -0500

    Add implementation of Project7

---
+++ Project7/Project7/Petition.swift
@@ -0,0 +1,16 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Ivan Erwin Lopez Ansaldo on 5/17/20.
+//  Copyright © 2020 Ivan Erwin Lopez Ansaldo. All rights reserved.
+//
+
+import Foundation
+
+struct Petition: Codable {
+  var title: String
+  var body: String
+  var signatureCount: Int
+}
+
---
+++ Project7/Project7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Ivan Erwin Lopez Ansaldo on 5/17/20.
+//  Copyright © 2020 Ivan Erwin Lopez Ansaldo. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+  var results: [Petition]
+}
